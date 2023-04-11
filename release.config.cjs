ff0e257815fc665f497e399cb65b5ed424726369
Joan Marc <joanmarc.ter@lovetoknow.com>
2023-04-11 17:56:17 +0200

    chore: update release config

--- release.config.cjs
+++ release.config.cjs
@@ -13,7 +13,7 @@ module.exports = {
       "@semantic-release/git",
       {
         "assets": ["docs/CHANGELOG.md"],
-        "message": "chore(release): ${nextRelease.version} [skip ci]\n\n${nextRelease.notes}"
+        "message": "chore(release): ${nextRelease.version}"
       },
     ],
     "@semantic-release/github",
