fcb6d943513b6384cb2ba27ed7890e21dd06da2c
Joan Marc <joanmarc.ter@lovetoknow.com>
2023-04-11 17:52:29 +0200

    fix: update multiply helper

--- main.js
+++ main.js
@@ -6,8 +6,8 @@ export function subtract(a, b) {
   return a - b;
 }
 
-export function multiply(a, b, c) {
-  return a * b * c;
+export function multiply(a, b) {
+  return a * b;
 }
 
 export function divide(a, b, c) {
